673809324b22b31a7130d9318b65b07b9e98c370
Indah Githa <indahgitha99@gmail.com>
2024-03-23 15:14:33 +0700

    home page umkm

--- lib/pages/umkm/home_page.dart
+++ lib/pages/umkm/home_page.dart
@@ -22,23 +22,30 @@ class _HomePageUMKMState extends State<HomePageUMKM> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            padding: EdgeInsets.symmetric(horizontal: 16),
+            height: 160,
+            decoration: BoxDecoration(
+              color: ColorStyles.primary,
+            ),
+            padding: EdgeInsets.only(top: 40, left: 16, right: 16),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Row(
                   children: [
-                    SvgPicture.asset('assets/foto-umkm.svg'),
+                    Image.asset('assets/umkm1.png', height: 48,),
                     SizedBox(width: 17),
                     Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
                           "Selamat Datang,",
                           style: GoogleFonts.poppins(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
                             color: ColorStyles.white,
                           ),
                         ),
@@ -49,15 +56,183 @@ class _HomePageUMKMState extends State<HomePageUMKM> {
                             fontWeight: FontWeight.w600,
                             color: ColorStyles.white,
                           ),
-                        )
+                        ),
+                        Text(
+                          "Lihat Profil > ",
+                          style: GoogleFonts.poppins(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: ColorStyles.white,
+                          ),
+                        ),
                       ],
                     ),
                   ],
                 ),
-                Icon(Icons.notifications, color: ColorStyles.white, size: 30),
+                Icon(Icons.notifications_outlined, color: ColorStyles.white, size: 30),
               ],
             ),
-          )
+          ),
+          SizedBox(height: 36),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Statistik Saham",
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: ColorStyles.primary,
+                  ),
+                ),
+                SizedBox(height: 19),
+                Container(
+                  decoration: BoxDecoration(
+                    color: ColorStyles.white,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 33),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Harga",
+                              style: GoogleFonts.poppins(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w400,
+                                color: ColorStyles.primary,
+                              ),
+                            ),
+                            Text(
+                              "3000/lbr",
+                              style: GoogleFonts.poppins(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: ColorStyles.black,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16,),
+                        child: Divider(
+                          color: ColorStyles.greyBg,
+                          thickness: 1,
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: ColorStyles.white,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 33),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Dividen",
+                                    style: GoogleFonts.poppins(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400,
+                                      color: ColorStyles.primary,
+                                    ),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      Text(
+                                        "2.5%",
+                                        style: GoogleFonts.poppins(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600,
+                                          color: ColorStyles.black,
+                                        ),
+                                      ),
+                                      Text(
+                                        "Rp75/lbr",
+                                        style: GoogleFonts.poppins(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w400,
+                                          color: ColorStyles.black,
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(horizontal: 16,),
+                              child: Divider(
+                                color: ColorStyles.greyBg,
+                                thickness: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: ColorStyles.white,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 33),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Total Terjual",
+                                    style: GoogleFonts.poppins(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400,
+                                      color: ColorStyles.primary,
+                                    ),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      Text(
+                                        "Rp30.000",
+                                        style: GoogleFonts.poppins(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600,
+                                          color: ColorStyles.black,
+                                        ),
+                                      ),
+                                      Text(
+                                        "30000 lbr",
+                                        style: GoogleFonts.poppins(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w400,
+                                          color: ColorStyles.black,
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
